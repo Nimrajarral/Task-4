05a6ec14a274510abeb15a09e2fa99569723493b
Nimrajarral <nimrajarral@gmail.com>
2024-03-13 16:58:57 +0500

    first commit

---
+++ task4.js
@@ -0,0 +1,7 @@
+//100 Days Challange Task ___  Day 1 Task 
+// Find a quote from a famous person you admire .Print the quote and the name of its author .Your output should look something like the following 
+//,including the quotation mark .
+//  Albert Einstien once said ," A person who never made a mistake never tried anythig more "
+var Author_name = ("Albert Einstien ");
+var quote = "A person who never made a mistake never tried anythig more ";
+console.log("".concat(Author_name, " once said, \"").concat(quote, "\""));
---
+++ task4.ts
@@ -0,0 +1,8 @@
+        //100 Days Challange Task ___  Day 1 Task 
+// Find a quote from a famous person you admire .Print the quote and the name of its author .Your output should look something like the following 
+//,including the quotation mark .
+//  Albert Einstien once said ," A person who never made a mistake never tried anythig more "
+
+let Author_name :string=("Albert Einstien ");
+let quote : string ="A person who never made a mistake never tried anythig more ";
+console.log(`${Author_name } once said, "${quote}"`);
